9f9de630d39369733e0ba8cd0f1eaadfe82bb062
Mary McInerney <57533857+mcinernm@users.noreply.github.com>
2023-08-14 16:42:25 -0400

    msg

---
+++ cypress/integration/examples/Test2.js
@@ -0,0 +1,16 @@
+/// <reference types = "Cypress" />
+
+describe('My Second Test Suite', function()  
+{
+    it('My FirstTest case', function() {
+
+    cy.visit("https://google.com")
+    
+
+
+
+
+
+    })
+
+})
